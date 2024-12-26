0b5311393ea67dc3f20bd1e7dd2ce8d3b198313a
HumayunMustafa <Humayun@msd-my.com>
2024-12-26 17:16:49 +0500

    adding context

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,3 +8,18 @@ Java_com_example_videogles_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_videogles_GLES3JNILIB_init(JNIEnv *env, jclass clazz) {
+    // TODO: implement init()
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_videogles_GLES3JNILIB_resize(JNIEnv *env, jclass clazz, jint width, jint height) {
+    // TODO: implement resize()
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_example_videogles_GLES3JNILIB_step(JNIEnv *env, jclass clazz) {
+    // TODO: implement step()
+}
